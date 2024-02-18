768b8ee1ea0671e3b8040fc6b4ecf48af9ab668a
Marek <ciesla.marek@gmail.com>
2024-02-18 15:54:53 +0100

    add fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: "#020916",
+        accent: "#CEFF7E",
+      },
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
